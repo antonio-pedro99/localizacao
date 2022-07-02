2737164cf85a9edf9641e16ae86921a0a975e2e6
Antonio Pedro <tonio.pedro99@gmail.com>
2022-07-03 03:25:46 +0530

    Add address, calculate thecost and distance"

--- samples/my_location/lib/services/coordinates_to_address.dart
+++ samples/my_location/lib/services/coordinates_to_address.dart
@@ -8,9 +8,8 @@ import 'package:http/http.dart' as http;
 const String localizacaoApi = "https://geolocalizacao-api.herokuapp.com";
 
 class CoordinateToAddress {
-
-
-  static Future<RequestResponse> fromCoordinates(double? latitude, double? longitude) async {
+  static Future<RequestResponse> fromCoordinates(
+      double? latitude, double? longitude) async {
     RequestResponse requestResponse = RequestResponse();
     await http
         .get(Uri.parse("$localizacaoApi/details/?lat=$latitude&lon=$longitude"))
@@ -25,17 +24,14 @@ class CoordinateToAddress {
     return requestResponse;
   }
 
-
-
   static Future<RequestResponse> calculatePriceByDistance(Query query) async {
     RequestResponse requestResponse = RequestResponse();
     await http
         .get(Uri.parse(
-            "$localizacaoApi/distance/cost/coordinates?loc_lact=28.525377&loc_lon=77.280106&des_lat=28.5481899&des_lon=77.2740645&fixed_tax=30&tax=3"))
+            "$localizacaoApi/distance/cost/coordinates?loc_lact=${query.locLatitude}&loc_lon=${query.locLongitude}&des_lat=${query.desLatitude}&des_lon=${query.desLongitude}&fixed_tax=40&tax_rate=4"))
         .then((value) {
       if (value.statusCode == 200) {
-        requestResponse.setData =
-            DefaultLocation.fromJson(jsonDecode(value.body));
+        requestResponse.setData = jsonDecode(value.body);
       } else {
         requestResponse.setError = jsonDecode(value.body);
       }
